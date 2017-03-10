6cd7700a8cd8703465f5b9b5ee461be81c869ecb
eilkers <ilker.riza.ernas@ericsson.com>
2017-03-10 16:50:58 +0200

    aspect is done

--- maven-dcjb-plugin/src/main/java/org/devside/ee/dcjb/plugin/JumpNextAspect.java
+++ maven-dcjb-plugin/src/main/java/org/devside/ee/dcjb/plugin/JumpNextAspect.java
@@ -1,9 +1,11 @@
 package org.devside.ee.dcjb.plugin;
 
+import java.io.IOException;
+import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.codec.binary.StringUtils;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.devside.ee.dcjb.client.annotation.JumpNextOne;
 
@@ -14,8 +16,6 @@ import javassist.CtMethod;
 import javassist.CtNewMethod;
 import javassist.NotFoundException;
 import javassist.bytecode.AccessFlag;
-import javassist.expr.ExprEditor;
-import javassist.expr.MethodCall;
 
 
 @Mojo(name="applyClientCode")
@@ -23,43 +23,58 @@ public class JumpNextAspect {
 
 	public static void main(String[] args) throws Exception {
 		
+		Class[] defaultExceptions = {Exception.class/*,SocketException.class,IOException.class*/};
 		boolean aspectApplied = false;
+		
 		ClassPool pool = ClassPool.getDefault();
 		pool.appendClassPath("/Users/rernas/git/remote-ejb-example/client/target/classes/");
 		CtClass cc = pool.get("com.illucit.ejbremote.TestEJB");
+		
 		for (CtMethod m : cc.getMethods()) {
 			JumpNextOne jumpNextOne = (JumpNextOne) m.getAnnotation(JumpNextOne.class);
 			if (jumpNextOne != null){
-				for (Class clazz : jumpNextOne.exceptions()) {
-					String methodName = m.getName();
-					m.setName(m.getName() + "Internal");
-					m.setModifiers(AccessFlag.PRIVATE);
-					int i=0;
-					String paramStr = "";
-					String varStr = "";
-					for(CtClass t :  m.getParameterTypes()){
-						if (i > 0 ){
-							paramStr += ",";
-							varStr += ",";
-						}
-						String v= "p" + i;
-						paramStr += t.getName() + " " + v;
-						varStr += v;
-						i++;
+				List<Class> exceptions =new ArrayList<Class>(); 
+//				exceptions.addAll(Arrays.asList(jumpNextOne.exceptions()));
+				exceptions.addAll(Arrays.asList(defaultExceptions));
+				
+				String methodName = m.getName();
+				m.setName(m.getName() + "Internal");
+				m.setModifiers(AccessFlag.PRIVATE);
+				int i=0;
+				String paramStr = "";
+				String varStr = "";
+				for(CtClass t :  m.getParameterTypes()){
+					if (i > 0 ){
+						paramStr += ",";
+						varStr += ",";
 					}
-					
-					
-					
-					String signature =  m.getReturnType().getName() + " " + methodName + "(" + paramStr + ")" ;
-					
-					CtMethod newMethod = CtNewMethod.make(
-			                 getModifier(m) + " " + signature +  " { "
-			                 		+ "System.out.println(\"$e\");"
-			                 		+ "return $proceed( " + varStr + "); }",
-			                 cc, "this", m.getName()	);
-					cc.addMethod(newMethod);
-					aspectApplied = true;
+					String v= "p" + i;
+					paramStr += t.getName() + " " + v;
+					varStr += v;
+					i++;
 				}
+				
+				
+				
+				String signature =  m.getReturnType().getName() + " " + methodName + "(" + paramStr + ")" ;
+				boolean isVoid = m.getReturnType().getName().equals("void");
+				String body =  getModifier(m) + " " + signature 
+                 		+ "{"
+                 		+ "boolean success=false;"
+                 		+ "boolean retry=true;"
+                 		+ (!isVoid?m.getReturnType().getName() + " retValue;":"") // Object retValue;
+                 		+ "while (retry){"
+                 		+ "try { "
+                 		+ (!isVoid?"retValue = ":"")  + "$proceed( " + varStr + ");"
+                 		+ "success=true;"
+                 		+ "retry=false;}"
+                 		+ addCatch(varStr, exceptions)
+                 		+ "}"
+                 		+ (!isVoid?"return retValue;":"")
+                 		+ "}";
+				CtMethod newMethod = CtNewMethod.make(body,cc, "this", m.getName()	);
+				cc.addMethod(newMethod);
+				aspectApplied = true;
 			}
 		}
 		if (aspectApplied)
@@ -83,10 +98,17 @@ public class JumpNextAspect {
 	}
 	
 	
-	public static void addCatch(CtMethod method, Class clazz) throws NotFoundException, CannotCompileException{
-		CtClass etype = ClassPool.getDefault().get(clazz.getName());
-		method.addCatch("{ System.out.println($e);}", etype);
+	public static String addCatch(String varStr,List<Class> clazzList) throws NotFoundException, CannotCompileException{
+		String catchStr = "";
+		for(Class clazz:clazzList){
+			catchStr += "catch(" + clazz.getName() + " e){ "
+								+ "retry=true; "
+						+ "}";
+		}
+		
+		return catchStr;
 	}
+	
 	
 	
 }
