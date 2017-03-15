e1c80bfed1ca227dc569568c9cf01998f7d6656c
eilkers <ilker.riza.ernas@ericsson.com>
2017-03-15 20:27:14 +0200

    recursive class search is addedd.

--- maven-dcjb-aspect-plugin/src/main/java/org/devside/ee/maven_dcjb_aspect_plugin/JumpNextAspect.java
+++ maven-dcjb-aspect-plugin/src/main/java/org/devside/ee/maven_dcjb_aspect_plugin/JumpNextAspect.java
@@ -31,71 +31,18 @@ public class JumpNextAspect extends AbstractMojo{
 	
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
 	MavenProject project;
-
+	
+	static ClassPool pool = ClassPool.getDefault();
+	static Class[] defaultExceptions = {Exception.class/*,SocketException.class,IOException.class*/};
+	
 	public  void execute() {
-		Class[] defaultExceptions = {Exception.class/*,SocketException.class,IOException.class*/};
-		boolean aspectApplied = false;
 		
-		CtClass cc = null;
 		try {
-			ClassPool pool = ClassPool.getDefault();
-			
-			System.out.println("asdasdsad");
-			pool.appendClassPath(project.getBuild().getDirectory() + File.separator + "classes");
-			cc = pool.get("com.illucit.ejbremote.TestEJB");
-			
-			for (CtMethod m : cc.getMethods()) {
-				JumpNextOne jumpNextOne = (JumpNextOne) m.getAnnotation(JumpNextOne.class);
-				if (jumpNextOne != null){
-					List<Class> exceptions =new ArrayList<Class>(); 
-					exceptions.addAll(Arrays.asList(jumpNextOne.exceptions()));
-					exceptions.addAll(Arrays.asList(defaultExceptions));
-					
-					String methodName = m.getName();
-					m.setName(m.getName() + "Internal");
-					m.setModifiers(AccessFlag.PRIVATE);
-					int i=0;
-					String paramStr = "";
-					String varStr = "";
-					for(CtClass t :  m.getParameterTypes()){
-						if (i > 0 ){
-							paramStr += ",";
-							varStr += ",";
-						}
-						String v= "p" + i;
-						paramStr += t.getName() + " " + v;
-						varStr += v;
-						i++;
-					}
-					
-					
-					
-					String signature =  m.getReturnType().getName() + " " + methodName + "(" + paramStr + ")" ;
-					boolean isVoid = m.getReturnType().getName().equals("void");
-					String body =  getModifier(m) + " " + signature 
-	                 		+ "{"
-	                 		+ "boolean success=false;"
-	                 		+ "boolean retry=true;"
-	                 		+ (!isVoid?m.getReturnType().getName() + " retValue;":"") // Object retValue;
-	                 		+ "while (retry){"
-	                 		+ "try { "
-	                 		+ (!isVoid?"retValue = ":"")  + "$proceed( " + varStr + ");"
-	                 		+ "success=true;"
-	                 		+ "retry=false;}"
-	                 		+ addCatch(varStr, exceptions)
-	                 		+ "}"
-	                 		+ (!isVoid?"return retValue;":"")
-	                 		+ "}";
-					CtMethod newMethod = CtNewMethod.make(body,cc, "this", m.getName()	);
-					cc.addMethod(newMethod);
-					aspectApplied = true;
-				}
-			}
-			if (aspectApplied)
-				cc.writeFile("/Users/rernas/git/remote-ejb-example/client/target/classes");
-			
-		} catch (NotFoundException e) {
-			getLog().error(e);
+			String path = project.getBuild().getDirectory() + File.separator + "classes";
+			pool.appendClassPath(path);
+			changeClass( path,"",path);
+		} catch (NotFoundException ee) {
+			getLog().error(ee);
 		} catch (IOException e) {
 			getLog().error(e);
 		} catch (CannotCompileException e) {
@@ -104,6 +51,80 @@ public class JumpNextAspect extends AbstractMojo{
 			getLog().error(e);
 		}
 		
+		
+		
+		
+	}
+
+	private void changeClass(String path,String pack, String basePath)
+			throws NotFoundException, ClassNotFoundException, CannotCompileException, IOException {
+		
+		File folder = new File(path);
+		File[] files = folder.listFiles();
+		for(File f:files){
+			if (f.isDirectory()){
+				if (!pack.isEmpty())
+					pack = pack + "." + f.getName();
+				else 
+					pack = f.getName();
+				changeClass(path + File.separator + f.getName(), pack , basePath);
+			}else if (f.getName().endsWith(".class")){
+				CtClass cc;
+				cc = pool.get(pack + "." + f.getName().substring(0,f.getName().indexOf(".class")));
+				
+				
+				boolean aspectApplied = false;
+				for (CtMethod m : cc.getDeclaredMethods()) {
+					JumpNextOne jumpNextOne = (JumpNextOne) m.getAnnotation(JumpNextOne.class);
+					if (jumpNextOne != null){
+						List<Class> exceptions =new ArrayList<Class>(); 
+						exceptions.addAll(Arrays.asList(jumpNextOne.exceptions()));
+						exceptions.addAll(Arrays.asList(defaultExceptions));
+						
+						String methodName = m.getName();
+						m.setName(m.getName() + "Internal");
+						m.setModifiers(AccessFlag.PRIVATE);
+						int i=0;
+						String paramStr = "";
+						String varStr = "";
+						for(CtClass t :  m.getParameterTypes()){
+							if (i > 0 ){
+								paramStr += ",";
+								varStr += ",";
+							}
+							String v= "p" + i;
+							paramStr += t.getName() + " " + v;
+							varStr += v;
+							i++;
+						}
+						
+						
+						
+						String signature =  m.getReturnType().getName() + " " + methodName + "(" + paramStr + ")" ;
+						boolean isVoid = m.getReturnType().getName().equals("void");
+						String body =  getModifier(m) + " " + signature 
+				         		+ "{"
+				         		+ "boolean success=false;"
+				         		+ "boolean retry=true;"
+				         		+ (!isVoid?m.getReturnType().getName() + " retValue;":"") // Object retValue;
+				         		+ "while (retry){"
+				         		+ "try { "
+				         		+ (!isVoid?"retValue = ":"")  + "$proceed( " + varStr + ");"
+				         		+ "success=true;"
+				         		+ "retry=false;}"
+				         		+ addCatch(varStr, exceptions)
+				         		+ "}"
+				         		+ (!isVoid?"return retValue;":"")
+				         		+ "}";
+						CtMethod newMethod = CtNewMethod.make(body,cc, "this", m.getName()	);
+						cc.addMethod(newMethod);
+						aspectApplied = true;
+					}
+				}
+				if (aspectApplied)
+					cc.writeFile(basePath);
+			}
+		}
 		
 		
 		
